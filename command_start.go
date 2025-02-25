1639a3e5665c8a71d5780f8a10bf92c84c94294e
gaurav <gaurav.mrc@gmail.com>
2025-02-25 10:47:54 +0530

    timer start and stop

--- command_start.go
+++ command_start.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
 func commandStart() error {
+	log.Printf("commandStart invoked ...")
+
+	stop := make(chan bool) // Channel to signal stopping timer
+	done := make(chan bool) // Channel to signal when everything is done
+	go timer(stop, done)
+	go listenToUserStop(stop)
+	<-done
+	log.Printf("commandStart done")
 	return nil
+}
+
+func timer(stop <-chan bool, done chan<- bool) {
+	log.Println("Timer function invoked ... ")
+	startTime := time.Now()
+	ticker := time.NewTicker(time.Second * 1)
+	defer ticker.Stop()
+
+	log.Printf("Starting session at : %v", startTime)
+	for {
+		select {
+		case t := <-ticker.C:
+			elapsedTime := t.Sub(startTime)
+			fmt.Printf("\rElapsed time : %v", formatDuration(elapsedTime))
+		case <-stop:
+			fmt.Println("Stopping timer.")
+			done <- true
+			log.Println("Timer function stopped")
+			return
+		}
+
+	}
+
+}
+
+func listenToUserStop(stop chan<- bool) {
+	log.Println("Listening for user input to stop timer ... ")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("Press 'z' and Enter to stop")
+		input, _ := reader.ReadString('\n')
+		if strings.ToLower(input) == "z\n" {
+			log.Println("User input received, stopping timer")
+			stop <- true
+			return
+		}
+	}
+
 }
---
+++ utils.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func formatDuration(d time.Duration) string {
+	hours := int(d.Hours())
+	minutes := int(d.Minutes()) % 60
+	seconds := int(d.Seconds()) % 60
+
+	if hours > 0 {
+		return fmt.Sprintf("%dh %dm %ds", hours, minutes, seconds)
+	} else if minutes > 0 {
+		return fmt.Sprintf("%dm %ds", minutes, seconds)
+	}
+	return fmt.Sprintf("%ds", seconds)
+}
